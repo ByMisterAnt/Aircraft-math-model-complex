713c3fb82f367e2bf07b03b268d617bb9fbf9301
ByMisterAnt <62688865+ByMisterAnt@users.noreply.github.com>
2021-08-01 01:28:48 +0300

    Delete planer.bat

--- planer.bat
+++
@@ -1,5 +0,0 @@
-D:
-cd D:\Matlab\FlightGear
-
-SET FG_ROOT=D:\Matlab\FlightGear\data
-.\\bin\fgfs --aircraft=757-200 --fdm=network,localhost,5501,5502,5503 --fog-fastest --disable-clouds --start-date-lat=2004:06:01:09:00:00 --disable-sound --in-air --enable-freeze --airport=KSFO --runway=10L --altitude=7224 --heading=113 --offset-distance=4.72 --offset-azimuth=0
