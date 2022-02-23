3e59abe6e9e13dc4209d2abc31adb29ad9e8c9c1
Ivan <62688865+ByMisterAnt@users.noreply.github.com>
2022-02-23 14:55:46 +0300

    added feedback control system

--- release/init.m
+++ release/init.m
@@ -2,14 +2,16 @@
 clear all
 clc
 close all
+
 %% def constants
-x0 = [100;0;0;0;0;0;0;0.1;0];
+
+x0 = [95;0;0;0;0;0;0;0.1;0];
 
 uu = [0; -0.1; 0; 0.0821; 0.0821];
 
-Tsim = 180;
+Tsim = Inf;
 
-%% min&max values
+%% min&max
 
 u1min = -25*pi/180;
 u1max = 25*pi/180;
@@ -25,3 +27,6 @@ u4max = 10*pi/180;
 
 u5min = -0.5*pi/180;
 u5max = 10*pi/180;
+
+xFlightGearVersion = 'v2017.1';
+SampleTime = 0.01;
